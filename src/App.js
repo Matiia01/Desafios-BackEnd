1d5fac78f05855ec663122d54471da6a94280170
Matiia01 <baigorria2019@hotmail.com>
2023-11-27 23:56:11 -0300

    v3

---
+++ src/App.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const ProductManager = require('./productManager'); 
+
+const app = express();
+const port = 8080;
+
+const path = require('path');
+const productManager = new ProductManager(path.join(__dirname, 'data.json'));
+
+app.get('/products', async (req, res) => {
+  try {
+    const limit = req.query.limit;
+    const products = await productManager.getProducts(limit);
+    res.json({ products });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/products/:pid', async (req, res) => {
+  try {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+    if (product) {
+      res.json({ product });
+    } else {
+      res.status(404).json({ error: 'Product not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
