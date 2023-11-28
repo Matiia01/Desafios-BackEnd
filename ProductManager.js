af4bff2e3566fbc9d4314fbcbdcf8c6b999e5dda
Matiia01 <baigorria2019@hotmail.com>
2023-11-27 23:55:13 -0300

    v2

--- ProductManager.js
+++ ProductManager.js
@@ -1,81 +1,92 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-      this.products = [];
-      this.productIdCounter = 1;
-    }
-  
-    addProduct(product) {
-      const { title, description, price, thumbnail, code, stock } = product;
-  
-      if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
-        console.log("Error: Todos los campos son obligatorios.");
-        return;
-      }
-  
-      if (this.products.some((p) => p.code === code)) {
-        console.log("Error: El código del producto ya existe.");
-        return;
-      }
-  
-      const newProduct = {
-        id: this.productIdCounter++,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      };
-  
-      this.products.push(newProduct);
-      console.log("Producto agregado satisfactoriamente.");
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find((p) => p.id === id);
-      if (product) {
-        return product;
-      } else {
-        console.log("Error: Producto no encontrado.");
-        return null;
-      }
+  constructor(filePath) {
+    this.path = filePath;
+    this.products = this.readProductsFromFile();
+  }
+
+  readProductsFromFile() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      return JSON.parse(data) || [];
+    } catch (error) {
+      return [];
     }
   }
-  
-  // Crear una instancia de la clase ProductManager
-  const productManager = new ProductManager();
-  
-  // Llamar al método getProducts, debe devolver un arreglo vacío []
-  console.log(productManager.getProducts());
-  
-  // Agregar un producto
-  productManager.addProduct({
-    title: "producto prueba",
-    description: "Este es un producto prueba",
-    price: 200,
-    thumbnail: "Sin imagen",
-    code: "abc123",
-    stock: 25,
-  });
-  
-  // Llamar al método getProducts nuevamente, esta vez debe aparecer el producto recién agregado
-  console.log(productManager.getProducts());
-  
-  // Intentar agregar un producto con el mismo código, debe arrojar un error
-  productManager.addProduct({
-    title: "producto repetido",
-    description: "Este producto tiene el mismo código",
-    price: 150,
-    thumbnail: "Otra imagen",
-    code: "abc123",
-    stock: 10,
-  });
-  
-  // Evaluar que getProductById devuelva un error si no encuentra el producto
-  const productoNoEncontrado = productManager.getProductById(100);
-  console.log(productoNoEncontrado);
-  
+
+  writeProductsToFile(products) {
+    fs.writeFileSync(this.path, JSON.stringify(products, null, 2), 'utf-8');
+  }
+
+  addProduct(product) {
+    const { title, description, price, thumbnail, code, stock } = product;
+
+    if (!title || !description || !price || !thumbnail || !code || stock === undefined) {
+      console.log("Error: Todos los campos son obligatorios.");
+      return;
+    }
+
+    if (this.products.some((p) => p.code === code)) {
+      console.log("Error: El código del producto ya existe.");
+      return;
+    }
+
+    const newProduct = {
+      id: this.getNextProductId(),
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+    };
+
+    this.products.push(newProduct);
+    this.writeProductsToFile(this.products);
+    console.log("Producto agregado satisfactoriamente.");
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.log("Error: Producto no encontrado.");
+      return null;
+    }
+  }
+
+  updateProduct(id, updatedProduct) {
+    const index = this.products.findIndex((p) => p.id === id);
+
+    if (index !== -1) {
+      this.products[index] = { ...this.products[index], ...updatedProduct };
+      this.writeProductsToFile(this.products);
+      console.log("Producto actualizado satisfactoriamente.");
+    } else {
+      console.log("Error: Producto no encontrado.");
+    }
+  }
+
+  deleteProduct(id) {
+    const updatedProducts = this.products.filter((p) => p.id !== id);
+
+    if (updatedProducts.length < this.products.length) {
+      this.writeProductsToFile(updatedProducts);
+      console.log("Producto eliminado satisfactoriamente.");
+    } else {
+      console.log("Error: Producto no encontrado.");
+    }
+  }
+
+  getNextProductId() {
+    return this.products.length > 0 ? Math.max(...this.products.map((p) => p.id)) + 1 : 1;
+  }
+}
+
+module.exports = ProductManager;
